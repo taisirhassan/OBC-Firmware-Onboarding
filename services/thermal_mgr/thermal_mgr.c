559f8656c8059113df5bb354f70a9a512274f8e0
taisirhassan <taisirisawesome@gmail.com>
2024-05-03 21:23:00 -0400

    Implemented Thermal management Tasks including ThermalMgr & ThermalMgrSendEvent Functions

--- services/thermal_mgr/thermal_mgr.c
+++ services/thermal_mgr/thermal_mgr.c
@@ -43,6 +43,15 @@ void initThermalSystemManager(lm75bd_config_t *config) {
 
 error_code_t thermalMgrSendEvent(thermal_mgr_event_t *event) {
   /* Send an event to the thermal manager queue */
+  // Check if the event is NULL
+  if(event == NULL) {
+    return ERR_CODE_INVALID_ARG;
+  }
+
+  // Send the event to the queue. If the queue is full, return an error code.
+if(xQueueSend(thermalMgrQueueHandle, event, 0) != pdTRUE) {
+    return ERR_CODE_QUEUE_FULL;
+  }
 
   return ERR_CODE_SUCCESS;
 }
@@ -54,9 +63,29 @@ void osHandlerLM75BD(void) {
 static void thermalMgr(void *pvParameters) {
   /* Implement this task */
   while (1) {
-    
+  // Create an event variable to store incoming events from the queue
+  thermal_mgr_event_t event;
+  // Cast the pvParameters to the configuration struct
+lm75bd_config_t *config = (lm75bd_config_t *)pvParameters;
+ if(xQueueReceive(thermalMgrQueueHandle, &event, portMAX_DELAY) == pdTRUE) {
+  // The type of the received event is checked.
+    switch (event.type) {
+      // If the event type is THERMAL_MGR_EVENT_MEASURE_TEMP_CMD, the task reads the current temperature from the LM75BD sensor.
+      case THERMAL_MGR_EVENT_MEASURE_TEMP_CMD:
+        // Read the temperature from the LM75BD
+        float tempC;
+        if (readTempLM75BD(config->devAddr, &tempC) == ERR_CODE_SUCCESS) {
+          addTemperatureTelemetry(tempC);
+        }
+        break;
+        // If the event type is not recognized, the task does nothing.
+      default:
+        break;
+    }
+  }
   }
 }
+   
 
 void addTemperatureTelemetry(float tempC) {
   printConsole("Temperature telemetry: %f deg C\n", tempC);
